8ada598feab076d31210fde46bda00d33a92871b
Janyson Marques <janyson29@hotmail.com>
2023-06-10 19:23:57 -0300

    join

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Plans from './components/plans/Plans';
+import Join from './components/Join/Join';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Reasons />
       <Plans />
       <Testimonials />
+      <Join />
     </div>
   );
 }
