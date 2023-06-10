1561409ba962f59cc0279e7ee9da94508b815e5d
Janyson Marques <janyson29@hotmail.com>
2023-06-10 20:47:50 -0300

    animação

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,8 +4,11 @@ import {testimonialsData} from '../../data/testimonialsData'
 import { useState } from 'react'
 import leftArrow from '../../assets/leftArrow.png'
 import rightArrow from '../../assets/rightArrow.png'
+import {motion} from 'framer-motion'
 
 const Testimonials = () => {
+  const transition = { type: 'spring', duration: 3};
+
   const [selected, setSelected] = useState(0);
   const tLength = testimonialsData.length;
 
@@ -16,9 +19,15 @@ const Testimonials = () => {
         <span>TESTIMONIALS</span>
         <span className='stroke-text'>WHAT THEY</span>
         <span>SAY ABOUT US</span>
-        <span>
+        <motion.span
+        key={selected}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0}}
+        exit={{ opacity: 0, x: 100}}
+        transition={transition}
+        >
           {testimonialsData[selected].review}
-        </span>
+        </motion.span>
         <span>
           <span style={{color: 'var(--orange)'}}>
             {testimonialsData[selected].name}
@@ -27,9 +36,23 @@ const Testimonials = () => {
         </span>
       </div>
       <div className="right-t">
-        <div></div>
-        <div></div>
-        <img src={testimonialsData[selected].image} alt="" />
+        <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ ...transition, duration: 2}}
+        whileInView={{ opacity: 1, x: 0}}
+        ></motion.div>
+        <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ ...transition, duration: 2}}
+        whileInView={{ opacity: 1, x: 0}}
+        ></motion.div>
+        <motion.img
+        key={selected}
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0}}
+        exit={{ opacity: 0, x: -100}}
+        transition={transition}
+        src={testimonialsData[selected].image} alt="" />
 
         <div className="arrows">
           <img 
